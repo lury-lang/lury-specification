bd601fc1bdd1153f32aa896ef92a6010a1ec617a
Nanase <nanase@users.noreply.github.com>
2016-10-27 03:37:32 +0900

    :bug: git initのタイミングを調整

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,9 @@ cd _book
 
 #--------------------------
 
+git init
 git config user.email "noreply@example.com"
 git config user.name "Travis-CI"
-
-git init
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@github.com/lury-lang/lury-specification.git" master:gh-pages > /dev/null 2>&1
